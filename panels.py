a8a7665219e7bd643c6a8b7a32ce0f73e8abee8b
dshot92 <dshot92@gmail.com>
2024-02-08 12:10:07 +0100

    feat: move to panel api of 4.1

--- panels.py
+++ panels.py
@@ -8,8 +8,6 @@ import bpy
 
 from .utils import is_ffmpeg_installed
 
-ffmpeg_installed = is_ffmpeg_installed()
-
 
 class RENDER_PT_RenderScriptInstances(bpy.types.Panel):
 
@@ -20,109 +18,60 @@ class RENDER_PT_RenderScriptInstances(bpy.types.Panel):
     bl_category = "Render Multiple Instances"
 
     def draw(self, context):
-        pass
-
-
-class RENDER_PT_OverrideFrameRange(bpy.types.Panel):
-    bl_label = "Override Frame Range"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = "WINDOW"
-    bl_parent_id = "RENDER_PT_RenderScriptInstances"
-    bl_options = {'DEFAULT_CLOSED'}
-
-    def draw_header(self, context):
-        props = context.scene.Render_Script_Props
-        self.layout.prop(props, "override_range", text="")
-
-    def draw(self, context):
-        props = context.scene.Render_Script_Props
-
-        layout = self.layout
-        layout.use_property_split = True
-
-        col = layout.column()
-        col.active = props.override_range
-        col.prop(props, "start_frame")
-        col.prop(props, "end_frame")
-
-
-class RENDER_PT_CreateRenderScript(bpy.types.Panel):
-    bl_label = "Render Instances"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = "WINDOW"
-    bl_parent_id = "RENDER_PT_RenderScriptInstances"
-
-    def draw(self, context):
-        props = context.scene.Render_Script_Props
-
-        layout = self.layout
-        layout.use_property_split = True
-
-        col = layout.column()
-
-        col.prop(props, "instances", text="Num Instances")
-        # col.prop(props, "auto_render", text="Launch Render Script")
-        col.operator(
-            'mesh.save_and_render',
-            text="Save and Render",
-            icon="RENDER_ANIMATION",
-        )
-
-
-class RENDER_PT_CreateVideoFile(bpy.types.Panel):
-    bl_label = "Sequence to .mp4"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = "WINDOW"
-    bl_parent_id = "RENDER_PT_RenderScriptInstances"
-
-    def draw(self, context):
-
         props = context.scene.Render_Script_Props
         layout = self.layout
         layout.use_property_split = True
 
-        col = layout.column()
+        # Frame Range Override panel
+        header, panel = layout.panel("panel_frames", default_closed=True)
+        header.label(text="Frame Range")
 
-        col.active = ffmpeg_installed
+        if panel:
 
-        if not col.active:
-            col = col.box()
-            col.label(text="FFmpeg is NOT installed")
-            col.label(text="Check Addon README.md for more info.")
-        else:
-            col.prop(props, "quality", text="High [1 -> 50] Low")
-            col.prop(props, 'encoder', text="Encoder")
-            col.prop(props, "mp4_file", text="Mp4 file")
+            panel.prop(props, "override_range", text="Override Frame Range")
+            panel.active = props.override_range
+            panel.prop(props, "start_frame")
+            panel.prop(props, "end_frame")
 
-            col.operator(
-                'mesh.ffmpeg_renders',
-                text="Create video from frames",
-                icon="SEQUENCE",
+        # Render Instances panel
+        header, panel = layout.panel("panel_render", default_closed=False)
+        header.label(text="Render Instances")
+
+        if panel:
+            panel.prop(props, "instances", text="Num Instances")
+            panel.operator(
+                'mesh.save_and_render',
+                text="Save and Render",
+                icon="RENDER_ANIMATION",
             )
 
+        # Sequence to mp4 panel
+        header, panel = layout.panel("panel_sequence", default_closed=False)
+        header.label(text="Sequence to .mp4")
 
-class RENDER_PT_OpenRenderFolder(bpy.types.Panel):
-    bl_label = "Open Render Folder"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = "WINDOW"
-    bl_parent_id = "RENDER_PT_RenderScriptInstances"
-    bl_options = {"HIDE_HEADER"}
+        if panel:
+            if not is_ffmpeg_installed():
+                panel.label(text="FFmpeg is NOT installed")
+                panel.label(text="Check Addon README.md for more info.")
+            else:
+                panel.prop(props, "quality", text="Quality")
+                panel.prop(props, 'encoder', text="Encoder")
+                panel.prop(props, "mp4_file", text="Mp4 save path")
 
-    def draw(self, context):
-        layout = self.layout
-        col = layout.column()
+                panel.operator(
+                    'mesh.ffmpeg_renders',
+                    text="Create video from frames",
+                    icon="SEQUENCE")
 
-        col.operator(
+        # Open render foldore button
+        layout.operator(
             'mesh.open_render_folder',
             text="Open render folder",
-            icon="FILE_FOLDER",
-        )
+            icon="FILE_FOLDER")
 
 
-classes = (RENDER_PT_RenderScriptInstances,
-           RENDER_PT_OverrideFrameRange,
-           RENDER_PT_CreateRenderScript,
-           RENDER_PT_CreateVideoFile,
-           RENDER_PT_OpenRenderFolder,)
+classes = (
+    RENDER_PT_RenderScriptInstances,
+)
 
 register, unregister = bpy.utils.register_classes_factory(classes)
