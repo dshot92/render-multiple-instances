016858593446e3ca5b432108384d1c25800eec53
dshot92 <dshot92@gmail.com>
2024-06-22 02:49:04 +0200

    feat: refactor code

--- operators.py
+++ operators.py
@@ -4,18 +4,20 @@
 # Author: Daniele Stochino (dshot92)
 # ----------------------------------------------------------
 
-import bpy
 import os
+import bpy
+import glob
 import subprocess
 
-from .preferences import getPreferences
 from .utils import (
-    isWindows,
-    isMacOS,
-    isLinux,
-    get_render_path,
-    get_render_command,
-    get_ffmpeg_command
+    is_windows,
+    is_linux,
+    is_macos,
+    get_export_dir,
+    get_blend_file,
+    get_export_parent_dir,
+    get_platform_terminal_command_list,
+    open_folder,
 )
 
 
@@ -28,71 +30,93 @@ class MESH_OT_Save_and_Render(bpy.types.Operator):
     bl_label = "Save and Render"
     bl_description = "Save blend file and start Renders"
 
+    def get_render_command_list(self, context):
+
+        props = bpy.context.scene.Render_Script_Props
+
+        blender_bin_path = bpy.app.binary_path  # Blender Executable Path
+
+        blend_file_path = get_blend_file()
+
+        start_frame = context.scene.frame_start
+        end_frame = context.scene.frame_end
+
+        # override_range = props.override_range
+
+        # Override if start_frame > end_frame and are > 0
+        if props.start_frame > props.end_frame:
+            start_frame = props.start_frame
+            end_frame = props.end_frame
+
+        # if override_range:
+        #     start_frame = props.start_frame
+        #     end_frame = props.end_frame
+
+        cmd = []
+        if is_windows():
+            cmd = [f'"{blender_bin_path}"', "-b",
+                   f'"{blend_file_path}"', "-s", f"{start_frame}", "-e",
+                   f"{end_frame}", "-a"]
+        elif is_macos():
+            cmd = [f'"{blender_bin_path}"', "--args", "-b",
+                   f'"{blend_file_path}"', "-s", f"{start_frame}", "-e",
+                   f"{end_frame}", "-a"]
+        elif is_linux():
+            cmd = [f'{blender_bin_path}', "-b",
+                   f'{blend_file_path}', "-s", f"{start_frame}", "-e",
+                   f"{end_frame}", "-a"]
+
+        return get_platform_terminal_command_list(cmd)
+
     def execute(self, context):
 
         file_ext = str(context.scene.render.image_settings.file_format).lower()
-        print(f"file_ext : {file_ext}")
 
-        if file_ext not in "png":
-            self.report(
-                {'ERROR'}, "File sequence must be in .png format")
+        if file_ext != "png" and file_ext != "jpg" and file_ext != "jpeg":
+            self.report({'ERROR'},
+                        "Export extension must be .png/.jpg")
             return {'CANCELLED'}
 
-        debug = getPreferences().debug
         props = bpy.context.scene.Render_Script_Props
 
-        use_overwrite = context.scene.render.use_overwrite
-        use_placeholder = context.scene.render.use_placeholder
+        # Store scene render settings
+        _use_overwrite = context.scene.render.use_overwrite
+        _use_placeholder = context.scene.render.use_placeholder
+        _resolution_percentage = context.scene.render.resolution_percentage
+
         context.scene.render.use_overwrite = False
         context.scene.render.use_placeholder = True
+        context.scene.render.resolution_percentage = props.res_percentage
 
         # Save file to ensure last changes are saved
         try:
             if not bpy.data.is_saved:
                 self.report(
-                    {'WARNING'}, "Blend file has never been saved before."
-                    + "Please save the file first.")
+                    {'WARNING'}, "Blend file has never been saved before. Please save the file first.")
                 return {'CANCELLED'}
             bpy.ops.wm.save_mainfile()
         except RuntimeError as e:
             self.report({'ERROR'}, f"Failed to save blend file: {e}")
             return {'CANCELLED'}
 
-        context.scene.render.use_overwrite = use_overwrite
-        context.scene.render.use_placeholder = use_placeholder
-
-        blender_path = bpy.app.binary_path  # Blender Executable Path
-        blend_file_path = bpy.data.filepath  # Blender File Path
-
-        start_frame = context.scene.frame_start
-        end_frame = context.scene.frame_end
-        override_range = props.override_range
-
-        if override_range:
-            start_frame = props.start_frame
-            end_frame = props.end_frame
-
         auto_render = props.auto_render
         instances = props.instances
 
-        if debug:
-            print("\n\nDebug MESH_OT_Create_script_file\n")
-            print(f"Blender path: {blender_path}")
-            print(f"Blender File path: {blend_file_path}")
+        cmd = self.get_render_command_list(context)
 
-        command = get_render_command(
-            blend_file_path,
-            blender_path,
-            start_frame,
-            end_frame
-        )
+        if auto_render:
+            try:
+                for _ in range(instances):
+                    subprocess.Popen(cmd)
+            except Exception as e:
+                self.report(
+                    {'ERROR'}, f"Failed to launch Blender instances: {e}")
+                return {'CANCELLED'}
 
-        if (auto_render):
-            for _ in range(instances):
-                os.system(command)
-
-        context.scene.render.use_overwrite = use_overwrite
-        context.scene.render.use_placeholder = use_placeholder
+        # Restore scene render settings
+        context.scene.render.use_overwrite = _use_overwrite
+        context.scene.render.use_placeholder = _use_placeholder
+        context.scene.render.resolution_percentage = _resolution_percentage
 
         self.report({'INFO'}, "Render script Created")
         return {'FINISHED'}
@@ -107,50 +131,104 @@ class MESH_OT_ffmpeg_renders(bpy.types.Operator):
     bl_label = "Create video from frames"
     bl_description = "Create video from frames"
 
-    def execute(self, context):
+    def get_mp4_output_path(self):
 
-        debug = getPreferences().debug
         props = bpy.context.scene.Render_Script_Props
 
-        # https://stackoverflow.com/questions/31201164/ffmpeg-error-pattern-type-glob-was-selected-but-globbing-is-not-support-ed-by
+        encoder = props.encoder
+        quality = props.quality
 
-        parent_render_path, render_filename, render_folder = get_render_path()
+        mp4_path = bpy.context.scene.Render_Script_Props.mp4_file
+        export_parent_dir = get_export_parent_dir()
+
+        blend_file = get_blend_file()
+        blend_file = os.path.splitext(blend_file)[0]
+
+        if mp4_path == "":
+            mp4_path = os.path.join(
+                export_parent_dir,
+                blend_file + f"_{encoder}_{quality}.mp4")
+
+        return mp4_path
+
+    def get_frame_list_path(self):
+
+        duration = 1 / bpy.context.scene.render.fps
+
+        export_dir = get_export_dir()
+        # export_path = utils.get_export_path()
+        frame_list_file = os.path.join(export_dir, "ffmpeg_input.txt")
+
+        # Use list comprehension to get both .png and .jpg files
+        files = [file for ext in ['*.png', '*.jpg']
+                 for file in glob.glob(os.path.join(export_dir, ext))]
+
+        files.sort()
+
+        try:
+            with open(frame_list_file, "w+") as outfile:
+                for file in files:
+                    outfile.write(f"file '{file}'\n")
+                    outfile.write(f"duration {duration}\n")
+            outfile.close()
+        except FileNotFoundError as e:
+            print('Error ', e)
+            print(f"The {frame_list_file} does not exist")
+
+        return frame_list_file
+
+    def get_ffmpeg_command_list(self):
+
+        # https://stackoverflow.com/questions/31201164/ffmpeg-error-pattern-type-glob-was-selected-but-globbing-is-not-support-ed-by
+        props = bpy.context.scene.Render_Script_Props
+
         encoder = props.encoder
         quality = props.quality
         fps = bpy.context.scene.render.fps
-        duration = 1 / bpy.context.scene.render.fps
-        output_file_path = props.mp4_file
 
-        if output_file_path == "":
-            output_file_path = os.path.join(
-                parent_render_path,
-                render_filename + f"_{encoder}_{quality}.mp4")
+        frame_list_file = self.get_frame_list_path()
+        output_file = self.get_mp4_output_path()
 
-        path_exists = os.path.isdir(render_folder)
+        cmd = []
+        # https://ntown.at/de/knowledgebase/cuda-gpu-accelerated-h264-h265-hevc-video-encoding-with-ffmpeg/
+        match encoder:
+            case "libx264":
+                # ffmpeg_command = f'ffmpeg -safe 0 -r {fps} -f concat
+                # -i "{frame_list_file}" -pix_fmt yuv420p -c:v {encoder}
+                # -crf {quality} -tune fastdecode "{output_file}" -y'
+                cmd = ["ffmpeg", "-safe", "0", "-r", f"{fps}", "-f",
+                       "concat", "-i", f'"{frame_list_file}"',
+                       "-pix_fmt", "yuv420p", "-c:v",
+                       f"{encoder}", "-crf", f"{quality}",
+                       f'"{output_file}"', "-y"]
+            case "libx265":
+                # ffmpeg -i input.mov -pix_fmt yuv420p -c:v libx265
+                # -crf 18 -tune fastdecode -g 1 output.mp4
+                cmd = ["ffmpeg", "-safe", "0", "-r", f"{fps}", "-f",
+                       "concat", "-i", f'"{frame_list_file}"', "-pix_fmt",
+                       "yuv420p", "-c:v", f"{encoder}", "-crf",
+                       f"{quality}", "-tune", "fastdecode", "-g", "1",
+                       f'"{output_file}"', "-y"]
+            case "libaom-av1":
+                # ffmpeg -i input.mov -pix_fmt yuv420p -c:v libx265 -crf 18
+                # -tune fastdecode -g 1 output.mp4
+                cmd = ["ffmpeg", "-strict", "-2", "-safe", "0", "-r",
+                       f"{fps}", "-f", "concat", "-i",
+                       f'"{frame_list_file}"', "-c:v", f"{encoder}",
+                       "-strict", "-2", "-crf", f"{quality}",
+                       f'"{output_file}"', "-y"]
 
-        if not path_exists:
+        return get_platform_terminal_command_list(cmd)
+
+    def execute(self, context):
+
+        if not os.path.isdir(get_export_dir()):
             self.report({'ERROR'}, "Render folder not found")
             return {'CANCELLED'}
 
-        ffmpeg_command = get_ffmpeg_command(
-            render_folder,
-            duration,
-            fps,
-            encoder,
-            quality,
-            output_file_path
-        )
+        cmd = self.get_ffmpeg_command_list()
 
-        os.system(ffmpeg_command)
-
-        if debug:
-            print("\n\nDebug MESH_OT_ffmpeg_renders\n")
-            print(f"Parent Render Path: {parent_render_path}")
-            print(f"Render Filename: {render_filename}")
-            print(f"Render Folder: {render_folder}")
-            print(f"Output File: {output_file_path}")
-            print(f"Encoder : {encoder}")
-            print(f"Command: {ffmpeg_command}")
+        os.system(' '.join(cmd))
 
         self.report({'INFO'}, "Video .mp4 encoded")
         return {'FINISHED'}
@@ -167,20 +245,13 @@ class MESH_OT_open_render_folder(bpy.types.Operator):
 
     def execute(self, context):
 
-        path, _, _ = get_render_path()
+        path = get_export_dir()
 
-        path_exists = os.path.isdir(path)
-
-        if not path_exists:
+        if not os.path.isdir(path):
             self.report({'ERROR'}, "Render folder not found")
             return {'CANCELLED'}
 
-        if isWindows():
-            os.startfile(path)
-        elif isMacOS():
-            subprocess.Popen(["open", path])
-        elif isLinux():
-            subprocess.Popen(["xdg-open", path])
+        open_folder(path)
 
         return {'FINISHED'}
 
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
+
 
 # ----------------------------------------------------------
 # Author: Daniele Stochino (dshot92)
@@ -9,19 +9,18 @@ import os
 import glob
 import platform
 import subprocess
+from pathlib import Path
 
-from .preferences import getPreferences
 
-
-def isWindows():
+def is_windows():
     return os.name == 'nt'
 
 
-def isMacOS():
+def is_macos():
     return os.name == 'posix' and platform.system() == "Dawriw"
 
 
-def isLinux():
+def is_linux():
     return os.name == 'posix' and platform.system() == "Linux"
 
 
@@ -29,10 +28,13 @@ def isLinux():
 enc = ["libx264", "libx265", "libaom-av1"]
 
 
-def make_executable(path):
-    mode = os.stat(path).st_mode
-    mode |= (mode & 0o444) >> 2    # copy R bits to X
-    os.chmod(path, mode)
+def open_folder(path):
+    if is_windows():
+        os.startfile(path)
+    elif is_macos():
+        subprocess.Popen(["open", path])
+    elif is_linux():
+        subprocess.Popen(["xdg-open", path])
 
 
 def is_ffmpeg_installed():
@@ -57,50 +59,57 @@ def get_encoders():
     return encoders
 
 
+def get_blend_file():
+    blend_file = bpy.data.filepath
+    return blend_file
+
+
+def get_export_dir():
+    filepath = bpy.path.abspath(bpy.context.scene.render.filepath)
+    return Path(filepath).resolve()
+
+
+def get_export_parent_dir():
+    export_path = get_export_dir()
+    return export_path.parent
+
+# Example of how to use these functions:
+
+
 def get_render_path():
-    path_render = (os.path.realpath(
-        bpy.path.abspath(bpy.context.scene.render.filepath)))
+    export_parent_dir = get_export_dir()
+    blend_file = get_blend_file()
+    export_path = get_export_dir()
+    return export_parent_dir, blend_file, export_path
 
-    last_char = bpy.context.scene.render.filepath[-1]
-    if last_char in "/\\":
-        path_render += os.sep
-        render_filename = path_render.split(os.sep)[-2]
-        parent_render_path = os.path.dirname(path_render)
-        parent_render_path = os.path.dirname(parent_render_path)
+
+def get_platform_terminal_command_list(command_list):
+    """Append command to platform and distro temirnal launching command"""
+    if is_windows():
+        return ["start", '""'] + command_list
+    elif is_macos():
+        return ["open", "-a", "Terminal.app", "--args"] + command_list
+    elif is_linux():
+        desktop_environment = os.environ.get('XDG_CURRENT_DESKTOP', '')
+        if desktop_environment == 'GNOME':
+            return ["gnome-terminal", "--"] + command_list
+        elif desktop_environment == 'KDE':
+            return ["konsole", "--hold", "-e"] + command_list
+        elif desktop_environment == 'XFCE':
+            return ["xfce4-terminal", "--command"] + command_list
+        else:
+            return ["x-terminal-emulator", "-e"] + command_list
     else:
-        render_filename = path_render.split(os.sep)[-2]
-        path_render = os.path.dirname(path_render)
-        parent_render_path = os.path.split(path_render)[0]
-
-    return parent_render_path, render_filename, path_render
-
-
-def get_render_command(blend_file_path, blender_path, start_frame, end_frame):
-
-    if isWindows():
-        command = ["start", '""', f'"{blender_path}"', "-b",
-                   f'"{blend_file_path}"', "-s", f"{start_frame}", "-e",
-                   f"{end_frame}", "-a"]
-    elif isMacOS():
-        command = ["open", "-a", f'"{blender_path}"', "--args", "-b",
-                   f'"{blend_file_path}"', "-s", f"{start_frame}", "-e",
-                   f"{end_frame}", "-a"]
-    elif isLinux():
-        term = getPreferences().terminal_emulator.split(" ")
-        term = " ".join(term)
-        command = [f"{term}", f"{blender_path}", "-b", f'"{blend_file_path}"',
-                   "-s", f"{start_frame}", "-e", f"{end_frame}", "-a", "&"]
-
-    return ' '.join(command)
+        raise RuntimeError("Unsupported platform")
 
 
 def get_frame_list(render_folder, duration):
 
     frame_list_file = os.path.join(render_folder, "ffmpeg_input.txt")
 
-    files = []
-    for file in glob.glob(render_folder + os.sep + "*.png"):
-        files.append(file)
+    # Use list comprehension to get both .png and .jpg files
+    files = [file for ext in ['*.png', '*.jpg']
+             for file in glob.glob(os.path.join(render_folder, ext))]
 
     files.sort()
 
@@ -115,69 +124,3 @@ def get_frame_list(render_folder, duration):
         print(f"The {frame_list_file} does not exist")
 
     return frame_list_file
-
-
-def get_ffmpeg_command(render_folder, duration, fps, encoder, quality, output_file):
-
-    frame_list_file = os.path.join(render_folder, "ffmpeg_input.txt")
-
-    files = []
-    for file in glob.glob(render_folder + os.sep + "*.png"):
-        files.append(file)
-
-    files.sort()
-
-    try:
-        with open(frame_list_file, "w+") as outfile:
-            for file in files:
-                outfile.write(f"file '{file}'\n")
-                outfile.write(f"duration {duration}\n")
-        outfile.close()
-    except FileNotFoundError:
-        print(f"The {frame_list_file} does not exist")
-
-    output_file = (os.path.realpath(bpy.path.abspath(output_file)))
-    if ".mp4" not in output_file:
-        output_file += ".mp4"
-
-    # https://ntown.at/de/knowledgebase/cuda-gpu-accelerated-h264-h265-hevc-video-encoding-with-ffmpeg/
-
-    if encoder == "libx264":
-        # ffmpeg_command = f'ffmpeg -safe 0 -r {fps} -f concat
-        # -i "{frame_list_file}" -pix_fmt yuv420p -c:v {encoder}
-        # -crf {quality} -tune fastdecode "{output_file}" -y'
-        ffmpeg_command = ["ffmpeg", "-safe", "0", "-r", f"{fps}", "-f",
-                          "concat", "-i", f'"{frame_list_file}"',
-                          "-pix_fmt", "yuv420p", "-c:v",
-                          f"{encoder}", "-crf", f"{quality}",
-                          f'"{output_file}"', "-y"]
-
-    elif encoder == "libx265":
-        # ffmpeg -i input.mov -pix_fmt yuv420p -c:v libx265
-        # -crf 18 -tune fastdecode -g 1 output.mp4
-        ffmpeg_command = ["ffmpeg", "-safe", "0", "-r", f"{fps}", "-f",
-                          "concat", "-i", f'"{frame_list_file}"', "-pix_fmt",
-                          "yuv420p", "-c:v", f"{encoder}", "-crf",
-                          f"{quality}", "-tune", "fastdecode", "-g", "1",
-                          f'"{output_file}"', "-y"]
-
-    elif encoder == "libaom-av1":
-        # ffmpeg -i input.mov -pix_fmt yuv420p -c:v libx265 -crf 18
-        # -tune fastdecode -g 1 output.mp4
-        ffmpeg_command = ["ffmpeg", "-strict", "-2", "-safe", "0", "-r",
-                          f"{fps}", "-f", "concat", "-i",
-                          f'"{frame_list_file}"', "-c:v", f"{encoder}",
-                          "-strict", "-2", "-crf", f"{quality}",
-                          f'"{output_file}"', "-y"]
-
-    # Convert list to string
-    ffmpeg_command = ' '.join(ffmpeg_command)
-
-    if isWindows():
-        ffmpeg_command = 'start "" ' + ffmpeg_command
-    elif isMacOS():
-        ffmpeg_command = 'open -a Terminal.app --args ' + ffmpeg_command
-    elif isLinux():
-        ffmpeg_command = ffmpeg_command + " &"
-
-    return ffmpeg_command
