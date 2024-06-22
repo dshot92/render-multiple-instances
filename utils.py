3518934199bf8c2e17024f6f4c774255fa924cf4
dshot92 <dshot92@gmail.com>
2024-06-22 03:29:25 +0200

    feat: OperatingSystem Class

--- utils.py
+++ utils.py
@@ -10,18 +10,25 @@ import glob
 import platform
 import subprocess
 from pathlib import Path
+from enum import Enum
 
 
-def is_windows():
-    return os.name == 'nt'
+class OperatingSystem(Enum):
+    WINDOWS = "Windows"
+    MACOS = "MacOS"
+    LINUX = "Linux"
+    UNKNOWN = "Unknown"
 
-
-def is_macos():
-    return os.name == 'posix' and platform.system() == "Dawriw"
-
-
-def is_linux():
-    return os.name == 'posix' and platform.system() == "Linux"
+    @staticmethod
+    def detect_os():
+        if os.name == 'nt':
+            return OperatingSystem.WINDOWS
+        elif os.name == 'posix' and platform.system() == "Darwin":
+            return OperatingSystem.MACOS
+        elif os.name == 'posix' and platform.system() == "Linux":
+            return OperatingSystem.LINUX
+        else:
+            return OperatingSystem.UNKNOWN
 
 
 # List of wanter encoders
@@ -29,15 +36,17 @@ enc = ["libx264", "libx265", "libaom-av1"]
 
 
 def open_folder(path):
-    if is_windows():
-        os.startfile(path)
-    elif is_macos():
-        subprocess.Popen(["open", path])
-    elif is_linux():
-        subprocess.Popen(["xdg-open", path])
+
+    match OperatingSystem.detect_os():
+        case OperatingSystem.WINDOWS:
+            os.startfile(path)
+        case OperatingSystem.MACOS:
+            subprocess.Popen(["open", path])
+        case OperatingSystem.LINUX:
+            subprocess.Popen(["xdg-open", path])
 
 
-def is_ffmpeg_installed():
+def is_ffmpeg_installed() -> bool:
     try:
         cmd = ["ffprobe", "-version"]
         _ = subprocess.check_output(cmd)
@@ -47,7 +56,7 @@ def is_ffmpeg_installed():
         return False
 
 
-def get_encoders():
+def get_encoders() -> list:
     cmd = ["ffprobe", "-encoders"]
     encoders_list = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
 
@@ -59,49 +68,47 @@ def get_encoders():
     return encoders
 
 
-def get_blend_file():
-    blend_file = bpy.data.filepath
+def get_blender_bin_path() -> Path:
+    blender_bin_path = Path(bpy.app.binary_path)
+    return blender_bin_path
+
+def get_blend_file() -> Path:
+    blend_file = Path(bpy.data.filepath)
     return blend_file
 
 
-def get_export_dir():
+def get_export_dir() -> Path:
     filepath = bpy.path.abspath(bpy.context.scene.render.filepath)
     return Path(filepath).resolve()
 
 
-def get_export_parent_dir():
+def get_export_parent_dir() -> Path:
     export_path = get_export_dir()
     return export_path.parent
 
-# Example of how to use these functions:
 
-
-def get_render_path():
-    export_parent_dir = get_export_dir()
-    blend_file = get_blend_file()
-    export_path = get_export_dir()
-    return export_parent_dir, blend_file, export_path
-
-
-def get_platform_terminal_command_list(command_list):
+def get_platform_terminal_command_list(command_list: list) -> list:
     """Append command to platform and distro temirnal launching command"""
-    if is_windows():
-        return ["start", '""'] + command_list
-    elif is_macos():
-        return ["open", "-a", "Terminal.app", "--args"] + command_list
-    elif is_linux():
-        d_env = os.environ.get('XDG_CURRENT_DESKTOP', '')
-        match d_env:
-            case 'GNOME':
-                return ["gnome-terminal", "--"] + command_list
-            case 'KDE':
-                return ["konsole", "--hold", "-e"] + command_list
-            case 'XFCE':
-                return ["xfce4-terminal", "--command"] + command_list
-            case _:
-                return ["x-terminal-emulator", "-e"] + command_list
-    else:
-        raise RuntimeError("Unsupported platform")
+    cmd = []
+    match OperatingSystem.detect_os():
+        case OperatingSystem.WINDOWS:
+            cmd = ["start", '""'] + command_list
+        case OperatingSystem.MACOS:
+            cmd = ["open", "-a", "Terminal.app", "--args"] + command_list
+        case OperatingSystem.LINUX:
+            match os.environ.get('XDG_CURRENT_DESKTOP', ''):
+                case 'GNOME':
+                    cmd = ["gnome-terminal", "--"] + command_list
+                case 'KDE':
+                    cmd = ["konsole", "--hold", "-e"] + command_list
+                case 'XFCE':
+                    cmd = ["xfce4-terminal", "--command"] + command_list
+                case _:
+                    cmd = ["x-terminal-emulator", "-e"] + command_list
+        case OperatingSystem.UNKNOWN:
+            raise RuntimeError("Unsupported platform")
+
+    return cmd
 
 
 def get_frame_list(render_folder, duration):
