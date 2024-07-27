757a16c8e48df51cefa402f24984baf10cd8e4c1
dshot92 <dshot92@gmail.com>
2024-07-27 17:11:03 +0200

    clean: remove unneeded code

--- __init__.py
+++ __init__.py
@@ -9,20 +9,7 @@ from . import (
     operators,
     panels
 )
-import sys
 
-# bl_info = {
-#     "name": "Render Multiple Instances",
-#     "version": (3, 0, 0),
-#     "author": "DShot92 <dshot92@gmail.com>",
-#     "blender": (4, 2, 0),
-#     "category": "Render",
-#     "location": "Output Properties > Render Multiple Instances",
-#     "description": "Render animations faster with multiple instances.",
-#     "warning": "",
-#     "doc_url": "https://github.com/dshot92/render-multiple-instances",
-#     "tracker_url": "",
-# }
 
 if "bpy" in locals():
     import importlib
@@ -38,14 +25,6 @@ if "bpy" in locals():
             importlib.reload(locals()[module])
 
 
-def cleanse_modules():
-    """search for your plugin modules in blender
-    python sys.modules and remove them"""
-    for module_name in sorted(sys.modules.keys()):
-        if module_name.startswith(__name__):
-            del sys.modules[module_name]
-
-
 def register():
     properties.register()
     operators.register()
@@ -56,5 +35,3 @@ def unregister():
     panels.unregister()
     operators.unregister()
     properties.unregister()
-
-    cleanse_modules()
